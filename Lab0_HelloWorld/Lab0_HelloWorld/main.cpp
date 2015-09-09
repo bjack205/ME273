ad2740e8ad126c855783fb4082e1ad1808076bde
Brian Jackson <bjack205@gmail.com>
2015-09-09 07:35:42 -0600

    Lab0

---
+++ Lab0_HelloWorld/Lab0_HelloWorld/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	char input;
+	cout << "Hello World!" << endl;
+	cout << "Enter any value to exit..." << endl;
+	cin >> input;
+	return 0;
+}
